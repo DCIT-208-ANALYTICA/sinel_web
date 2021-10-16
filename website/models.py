46ee4f83fa87975b4cc151a5a2abc3c12b7fc048
yawa <yawsarfo2019@gmail.com>
2021-10-16 15:26:13 +0000

    add AdminData class to models.py file

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class AdminData(model.Models):
+    Username=models.CharField(max_length=100)
+    Email_Adress=models.EmailField()
+    Fullname=models.CharField(max_length=100)
+    Is_Super_User=models.BooleanField(default=False)
+
+    
