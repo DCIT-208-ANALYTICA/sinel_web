82c777d20bd45d3ea7d03bd071f97bc5151054e1
yawa <yawsarfo2019@gmail.com>
2021-10-16 15:37:01 +0000

    add Telophone and LatLng class variable to Contact_Data_Entity class

--- website/models.py
+++ website/models.py
@@ -8,11 +8,13 @@ class AdminData(model.Models):
     Fullname=models.CharField(max_length=100)
     Is_Super_User=models.BooleanField(default=False)
 
-# Contact_Data_Entity
+# Contact_Data_Entity 
 class Contact_Data_Entity(model.Models):
     Ghana_Post_Service=models.CharField(max_length=100)
     Email_Adress=models.EmailField()
     Physical_Address=models.CharField(max_length=100)
+    Telephone=models.CharField(max_length=100)
+    LatLng=models.CharField(max_length=100)
     
 
     
