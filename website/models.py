b5802e1ee269925ae11a5fe36f9792c5c2937672
yawa <yawsarfo2019@gmail.com>
2021-10-16 15:31:45 +0000

    add Contact_Data_Entity class to models.py file

--- website/models.py
+++ website/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
-
+# Admin Data Entry
 class AdminData(model.Models):
     Username=models.CharField(max_length=100)
     Email_Adress=models.EmailField()
     Fullname=models.CharField(max_length=100)
     Is_Super_User=models.BooleanField(default=False)
+
+# Contact_Data_Entity
+class Contact_Data_Entity(model.Models):
+    Ghana_Post_Service=models.CharField(max_length=100)
+    Email_Adress=models.EmailField()
+    Physical_Address=models.CharField(max_length=100)
+    
 
     
