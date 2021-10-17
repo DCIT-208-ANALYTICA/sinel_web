afd262dca28585ba1b5aefceb8c0f2baa09d8f69
mikekwabs <mikekobby10@gmail.com>
2021-10-17 00:34:17 +0000

    effected corrections made by raynard

--- website/models.py
+++ website/models.py
@@ -1,103 +1,128 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
-# Admin Data Entry
-class AdminData(model.Models):
-    Username=models.CharField(max_length=100)
-    Email_Adress=models.EmailField()
-    Fullname=models.CharField(max_length=100)
-    Is_Super_User=models.BooleanField(default=False)
 
-# Contact_Data_Entity 
-class Contact_Data_Entity(model.Models):
-    Ghana_Post_Service=models.CharField(max_length=100)
-    Email_Adress=models.EmailField()
-    Physical_Address=models.CharField(max_length=100)
-    Telephone=models.CharField(max_length=100)
-    LatLng=models.CharField(max_length=100)
+DAYS_OF_WEEK = (
+    (0, "Monday"),
+    (1, "Tuesday"),
+    (2, "Wednesday"),
+    (3, "Thursday"),
+    (4, "Friday"),
+    (5, "Saturday"),
+    (6, "Sunday"),
+)
+#
 
-#Social_Media_Link
-class Social_Media_Link(model.Models):
-    Name=models.CharField(max_length=100)
-    Icon_Url=models.URLField(max_length=200)
-    Link=models.CharField(max_length=100)
+
+class ContactDataEntity(model.Models):
+    gps = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20)
+    long = models.FloatField()
+    lat = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class SocialMediaLink(model.Models):
+    name = models.CharField(max_length=100)
+    icon_url = models.URLField(max_length=200)
+    link = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 # Insurance Partner
-class Insurance_Partner(model.Models):
-    Name=models.CharField(max_length=100)
-    Description=models.CharField(max_length=200)
-    LOGO=models.URLField(max_length=200)
-    Visible=models.BooleanField(default=False)
+class InsurancePartner(model.Models):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    logo = models.URLField(max_length=200)
+    visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 # Gallery
 class Gallery(model.Models):
-    Title=models.CharField(max_length=100)
-    Description=models.CharField(max_length=200)
-    Type=models.CharField(max_length=200)
-    URL=models.URLField(max_length=200)
-    Visible=models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 # Healt-Tips
-class Health_Tips(model.Models):
-    Title=models.CharField(max_length=100)
-    Slug=models.CharField(max_length=100)
-    Content=models.CharField(max_length=200)
+class HealthTips(model.Models):
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# Banner
+
 class Banner(model.Models):
-    Title=models.CharField(max_length=100)
-    Description=models.CharField(max_length=200,null=True)
-    Type=models.CharField(max_length=200)
-    URL=models.URLField(max_length=200,null=True)
-    Image=models.ImageField(max_length=200)
-    Visible=models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True)
+    url = models.URLField(max_length=200, null=True)
+    image = models.ImageField(
+        upload_to=None, height_field=None, width_field=None, max_length=100
+    )
+    visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# Services
-class Services(model.Models):
-    Title=models.CharField(max_length=100)
-    Description=models.CharField(max_length=200,null=True)
-    Image=models.ImageField(max_length=200)
-    Visible=models.BooleanField(default=False)
 
-# client
+class Services(Model.Models):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True)
+    image = models.ImageField(
+        upload_to=None, height_field=None, width_field=None, max_length=100
+    )
+    visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Client(model.Models):
-    Story=models.CharField(max_length=500)
-    Description=models.CharField(max_length=200)
-    Image=models.ImageField(max_length=200)
-    Visible=models.BooleanField(default=False)
+    title = models.CharField(max_length=30)
+    story = models.TextField()
+    image = models.ImageField(
+        upload_to=None, height_field=None, width_field=None, max_length=100
+    )
+    visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 # Open hour
-class open_hour(model.Models):
-    DAYS=models.CharField(max_length=500)
-    time=models.CharField(max_length=100)
-    
+class OpeningHour(model.Models):
+    days = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
+    time = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 # Team_lead
-class Team_Lead(model.Models):
-    name=models.CharField(max_length=100)
-    Bio=name=models.CharField(max_length=500)
-    image=models.ImageField(max_length=200)
-    Visible=models.BooleanField(default=False)
+class TeamLead(model.Models):
+    name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500)
+    image = models.ImageField(
+        upload_to=None, height_field=None, width_field=None, max_length=100
+    )
+    Visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 # About
 class About(model.Models):
-    Overview=models.CharField(max_length=200)
-    Mission=models.CharField(max_length=500)
-    Vision=models.CharField(max_length=500)
-    Value=models.CharField(max_length=100)
-
-    
-
-
-     
-
-
-
-
-
-   
-   
-    
-
-    
-
-    
+    overview = models.CharField(max_length=200)
+    mission = models.CharField(max_length=500)
+    vision = models.CharField(max_length=500)
+    value = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
