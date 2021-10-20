b36f9607447c909e02dd9149d4c3e3198c951d2c
samuelships <samuelshipss@gmail.com>
2021-10-20 13:09:20 +0000

    cleanup header

--- dashboard/react/src/App.js
+++ dashboard/react/src/App.js
@@ -14,40 +14,17 @@ import NonAuthLayout from "./components/NonAuthLayout";
 // Import scss
 import "./theme.scss";
 
-//Fake backend
-import fakeBackend from './helpers/AuthType/fakeBackend';
-
-//Firebase helper
-import { initFirebaseBackend } from "./helpers/firebase_helper";
-
-// Activating fake backend
-// fakeBackend();
-
-// const firebaseConfig = {
-// 	apiKey: process.env.REACT_APP_APIKEY,
-// 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
-// 	databaseURL: process.env.REACT_APP_DATABASEURL,
-// 	projectId: process.env.REACT_APP_PROJECTID,
-// 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-// 	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-// 	appId: process.env.REACT_APP_APPID,
-// 	measurementId: process.env.REACT_APP_MEASUREMENTID,
-// };
-  
-// init firebase backend
-// initFirebaseBackend(firebaseConfig);
-
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {  };
+		this.state = {};
 		this.getLayout = this.getLayout.bind(this);
 	}
- 
- 	/**
-	 * Returns the layout
-	 */
+
+	/**
+   * Returns the layout
+   */
 	getLayout = () => {
 		let layoutCls = VerticalLayout;
 
@@ -59,7 +36,7 @@ class App extends Component {
 				layoutCls = VerticalLayout;
 				break;
 		}
-		console.log(this.props);
+
 		return layoutCls;
 	};
 
