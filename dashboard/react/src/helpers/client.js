3e8071d681f913a7bc8c5d6ff188195c1bb64721
samuelships <samuelshipss@gmail.com>
2021-11-21 16:05:05 +0000

    add : contact crud

--- dashboard/react/src/helpers/client.js
+++ dashboard/react/src/helpers/client.js
@@ -21,6 +21,37 @@ class APIClient {
 
         return res;
     }
+
+    async getContact() {
+        const token = JSON.parse(localStorage.getItem("authUser")).token;
+        const res = await this.instance.request({
+            url: "api/v1/contact",
+            headers: { 'Content-Type': 'application/json' },
+            method: "get",
+            headers: {
+                "Authorization": "Token " + token
+            }
+
+        });
+
+        return res;
+    }
+
+    async updateContact(data) {
+        const token = JSON.parse(localStorage.getItem("authUser")).token;
+        const res = await this.instance.request({
+            url: "api/v1/contact",
+            headers: { 'Content-Type': 'application/json' },
+            method: "put",
+            headers: {
+                "Authorization": "Token " + token,
+                "Content-Type": "application/json"
+            },
+            data: data
+        });
+
+        return res;
+    }
 }
 
 export default APIClient;
