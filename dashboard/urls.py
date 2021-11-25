3fbbc983938dd67a9927c6734847d1a89077325f
mikekwabs <mikekobby10@gmail.com>
2021-11-25 13:41:25 +0000

    added views to dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,12 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    path("", views.About.as_view(), name="about"),
+    path("", views.Gallery.as_view(), name="gallery"),
+    path("", views.Services.as_view(), name="services"),
+    path("", views.TeamLeads.as_view(), name="teamleads"),
+    path("", views.Contact.as_view(), name="contact"),
+    path("", views.Clients.as_view(), name="clients"),
+    path("", views.Blog.as_view(), name="blog"),
+    path("", views.Banners.as_view(), name="banners"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -9,3 +9,75 @@ class Index(View):
         context = {}
 
         return render(request, self.template_name, context)
+
+
+class About(View):
+    template_name = "dashboard/about.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
+
+
+class Contact(View):
+    template_name = "dashboard/contact.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
+
+
+class Gallery(View):
+    template_name = "dashboard/gallery.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
+
+
+class Services(View):
+    template_name = "dashboard/services.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
+
+
+class TeamLeads(View):
+    template_name = "dashboard/teamleads.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
+
+
+class Banners(View):
+    template_name = "dashboard/banners.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
+
+
+class Clients(View):
+    template_name = "dashboard/clients.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
+
+
+class Blog(View):
+    template_name = "dashboard/blog.html"
+
+    def get(self, request, *argd, **kwargs):
+        context = {}
+
+        return render(request, self.template_name, context)
