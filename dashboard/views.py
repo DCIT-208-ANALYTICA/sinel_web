ad359a38342ca0a6fcf43731bff63c9ad833c475
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-11-25 16:10:09 +0000

    updated dashboard about page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 from sinel_web.utils.decorators import staff_only
+from website.models import About
 
 
-class Index(View):
+class IndexView(View):
     template_name = "dashboard/index.html"
 
     @method_decorator(staff_only("accounts:login"))
@@ -13,72 +14,100 @@ class Index(View):
 
         return render(request, self.template_name, context)
 
-class About(View):
+class AboutView(View):
     template_name = "dashboard/about.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
-        context = {}
-
+        context = {
+            "about":About.objects.first()
+        }
         return render(request, self.template_name, context)
 
+    @method_decorator(staff_only("accounts:login"))
+    def post(self, request, *argd, **kwargs):
+        overview = request.POST.get("overview-content")
+        mission = request.POST.get("mission-content")
+        vision = request.POST.get("vission-content")
+        value = request.POST.get("value-content")
 
-class Contact(View):
+        about = About.objects.first()
+        if overview:
+            about.overview = overview
+        if mission:
+            about.mission = mission
+        if vision:
+            about.vision = vision
+        if value:
+            about.value = value
+        about.save()
+        return redirect(request.META.get('HTTP_REFERER'))
+
+
+class ContactView(View):
     template_name = "dashboard/contact.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
         context = {}
 
         return render(request, self.template_name, context)
 
 
-class Gallery(View):
+class GalleryView(View):
     template_name = "dashboard/gallery.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
         context = {}
 
         return render(request, self.template_name, context)
 
 
-class Services(View):
+class ServicesView(View):
     template_name = "dashboard/services.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
         context = {}
 
         return render(request, self.template_name, context)
 
 
-class TeamLeads(View):
+class TeamLeadsView(View):
     template_name = "dashboard/teamleads.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
         context = {}
 
         return render(request, self.template_name, context)
 
 
-class Banners(View):
+class BannersView(View):
     template_name = "dashboard/banners.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
         context = {}
 
         return render(request, self.template_name, context)
 
 
-class Clients(View):
+class ClientsView(View):
     template_name = "dashboard/clients.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
         context = {}
 
         return render(request, self.template_name, context)
 
 
-class Blog(View):
+class BlogView(View):
     template_name = "dashboard/blog.html"
 
+    @method_decorator(staff_only("accounts:login"))
     def get(self, request, *argd, **kwargs):
         context = {}
 
