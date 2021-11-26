64588019ca24b138dd86b01291ff1597aa687595
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-11-26 10:24:28 +0000

    update

--- communications/models.py
+++ communications/models.py
@@ -6,7 +6,7 @@ class Appointment(models.Model):
     fullname = models.CharField(max_length=70)
     email = models.EmailField(max_length=254)
     number = models.CharField(max_length=13) #Incase of +233
-    date  = models.DateField()
+    date  = models.DateField(auto_now_add=True)
     message = models.TextField()
     service = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
