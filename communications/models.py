68a7d346f005fc86b87e57f12f418c90bf3ec1ed
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-11-26 10:32:03 +0000

    update

--- communications/models.py
+++ communications/models.py
@@ -10,3 +10,6 @@ class Appointment(models.Model):
     service = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.fullname
