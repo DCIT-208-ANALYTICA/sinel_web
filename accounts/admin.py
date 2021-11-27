96879020372080080619e1b7fd3c2ffded1450ed
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-11-27 04:38:50 +0000

    auto creation of default super admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,3 +3,13 @@ from . models import Administrator
 
 # Register your models here.
 admin.site.register(Administrator)
+
+# Create default superuser
+try:
+    superadmin, created = Administrator.objects.get_or_create(email_address="admin@email.com", is_superuser=True)
+    print("created", created)
+    if created:
+        superadmin.set_password("admin")
+        print("Super user created")
+except Exception as e:
+    print("Failed to create defautl superuser.: " + str(e))
