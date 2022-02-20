85db4ae168a8f4103381d7f7b6c1d0db0c664e2a
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2022-02-20 16:25:57 +0000

    enter DoB when requesting appoitment

--- communications/models.py
+++ communications/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Appointment(models.Model):
     fullname = models.CharField(max_length=70)
-    email = models.EmailField(max_length=254)
-    number = models.CharField(max_length=13) #Incase of +233
-    date  = models.DateField(auto_now_add=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    number = models.CharField(max_length=13)  #Incase of +233
+    date = models.DateField(auto_now_add=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     message = models.TextField()
     service = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
